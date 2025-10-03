6c1e292cbc1d875ae57929f2c721be3aae81dc50
Rachael Chen <rchen2027@berkeley.edu>
2025-10-02 23:35:49 -0700

    tell ESLint to allow node globals, delete unused container, add missing script

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,5 @@
 import { Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
