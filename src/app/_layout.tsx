04329df41e8371b54b4540c50aa85717c733ab9b
Torin Nayak <torinnayak@berkeley.edu>
2025-10-13 17:57:29 -0700

    Select an NPO Screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,8 @@
 import { Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import OnbordingFlow from './onboarding/page';
+import OnboardingFlow from './onboarding/page';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,8 +18,9 @@ function IndexScreen() {
 function StackLayout() {
   return (
     <SafeAreaProvider>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Onboarding">
         <Stack.Screen name="Index" component={IndexScreen} />
+        <Stack.Screen name="Onboarding" component={OnboardingFlow} />
       </Stack.Navigator>
     </SafeAreaProvider>
   );
---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Alert, Text, View } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { Button } from 'react-native-paper';
+
+export default function OnboardingFlow() {
+  const MOCK_NPOS = [
+    { id: 1, name: 'Replate' },
+    { id: 2, name: 'Adopt an Inmate' },
+    { id: 3, name: 'Amigos De Los Rios' },
+    { id: 4, name: 'Rose Academies' },
+    { id: 5, name: 'EcoVet' },
+  ];
+
+  const [open, setOpen] = useState(false);
+  const [selectedNPOId, setSelectedNPOId] = useState(null);
+  const [items, setItems] = useState(
+    MOCK_NPOS.map(npo => ({ label: npo.name, value: npo.id })),
+  );
+
+  return (
+    <View style={{ marginTop: 50 }}>
+      <Text>Which NPO are you partnered with?</Text>
+
+      <DropDownPicker
+        open={open}
+        value={selectedNPOId}
+        items={items}
+        setOpen={setOpen}
+        setValue={setSelectedNPOId}
+        setItems={setItems}
+        placeholder="Select an NPO"
+      />
+
+      <Button
+        mode="contained"
+        disabled={selectedNPOId === null}
+        onPress={() => Alert.alert('Your NPO selection has been saved.')}
+      >
+        Finish
+      </Button>
+    </View>
+  );
+}
