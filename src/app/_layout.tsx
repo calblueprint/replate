c9e70734d1db44de556770ba4549b3deffc12e79
Rachael Chen <rchen2027@berkeley.edu>
2025-11-03 13:43:14 -0800

    remove header

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,18 +2,7 @@ import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerTitle: "Today's Tasks",
-        headerStyle: { backgroundColor: '#DCDCDC' },
-        headerShadowVisible: false,
-        headerTitleStyle: {
-          fontSize: 22,
-          fontWeight: '700',
-        },
-        contentStyle: { backgroundColor: '#DCDCDC' },
-      }}
-    >
+    <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="onboarding" options={{ headerShown: false }} />
       <Stack.Screen name="pickup-details" options={{ headerShown: false }} />
